d7ac929a4dcb899a00f19a56cc9bc877b454cf28
JakobBundgaard <jakobbundgaard@hotmail.com>
2022-09-26 15:31:30 +0200

    small cleanup

--- BT_PHP_MVC/bundgaardmvc/app/views/includes/footer.php
+++
@@ -1,6 +0,0 @@
-
-    <script src="<?php echo URL_ROOT; ?>/js/main.js"></script>
-
-
-</body>
-</html>
--- shareposts/public/index.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-    require_once '../app/bootstrap.php';
-
-    // Initiate Core Library
-    $init = new Core;
-
-?>
